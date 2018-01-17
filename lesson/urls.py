106bb7316563c94d63d019ef3b782ba541d4ec1d
zouyingjie <yingjiezou1@gmail.com>
2018-01-17 17:08:28 +0800

    update url

--- lesson/urls.py
+++ lesson/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 
-from lesson.views import HelloView, AnsibleLessonView, HelloAnsibleView, HelloSuperView
+from lesson.views import HelloView, AnsibleLessonView, HelloSuperView, HelloAnsibleView
 
 urlpatterns = [
     url('^hello/$', HelloView.as_view(), name="helle_view"),
     url('^ansible/$', AnsibleLessonView.as_view(), name="ansible_view"),
-    url('^hello/ansible/$', HelloAnsibleVietpuw.as_view(), name="hello_ansible_view"),
+    url('^hello/ansible/$', HelloAnsibleView.as_view(), name="hello_ansible_view"),
     url('^hello/super/$', HelloSuperView.as_view(), name="hello_super_view"),
 ]
