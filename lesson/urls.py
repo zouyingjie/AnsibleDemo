5056cda13715f3f3bd5d7789e30de63bdfeb067b
zouyingjie <yingjiezou1@gmail.com>
2018-01-17 17:13:10 +0800

    update url

--- lesson/urls.py
+++ lesson/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url('^hello/$', HelloView.as_view(), name="helle_view"),
     url('^ansible/$', AnsibleLessonView.as_view(), name="ansible_view"),
     url('^hello/ansible/$', HelloAnsibleView.as_view(), name="hello_ansible_view"),
-    url('^hello/super/$', HelloSuperView.as_view(), name="hello_super_view"),
+    url('^hello/hhh/$', HelloSuperView.as_view(), name="hello_super_view"),
 ]
